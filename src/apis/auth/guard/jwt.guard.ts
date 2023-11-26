30119b0b97cd401cbeb3e7488c9f04e267607b9a
ehdtjs0612 <inko51366@naver.com>
2023-11-26 20:02:03 +0900

    refactor: jwt guard에서 필요없는 오버라이딩 제거

--- src/apis/auth/guard/jwt.guard.ts
+++ src/apis/auth/guard/jwt.guard.ts
@@ -1,10 +1,7 @@
-import { ExecutionContext, Injectable } from "@nestjs/common";
+import { ExecutionContext, Injectable, UnauthorizedException } from "@nestjs/common";
+import { TokenExpiredError } from "@nestjs/jwt";
 import { AuthGuard } from "@nestjs/passport";
 import { Observable } from "rxjs";
 
 @Injectable()
-export class JwtAuthGuard extends AuthGuard('jwt') {
-	canActivate(context: ExecutionContext): boolean | Promise<boolean> | Observable<boolean> {
-		return super.canActivate(context);
-	}
-}
+export class JwtAuthGuard extends AuthGuard('jwt') { }
