33931251a2782030177711b5dc39b4276efa9b67
ehdtjs0612 <inko51366@naver.com>
2023-11-26 20:17:27 +0900

    feat: nestjs config모듈 이용해보기

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,18 +7,24 @@ import { UsersModule } from './apis/users/users.module';
 import { AuthModule } from './apis/auth/auth.module';
 import { APP_INTERCEPTOR } from '@nestjs/core';
 import { PostsModule } from './apis/posts/posts.module';
+import { CommonModule } from './common/common.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
   imports: [
     TypeOrmModule.forRoot(
       typeORMConfig
     ),
+    ConfigModule.forRoot({
+      isGlobal: true
+    }),
     UsersModule,
     AuthModule,
-    PostsModule
+    PostsModule,
+    CommonModule
   ],
   controllers: [AppController],
-  providers: [AppService, {
+  providers: [AppService, ConfigService, {
     provide: APP_INTERCEPTOR,
     useClass: ClassSerializerInterceptor
   }],
