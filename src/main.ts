c6f854eac8ce23ef44efcdfd7cf9b3363acd219e
ehdtjs0612 <inko51366@naver.com>
2023-11-30 17:03:06 +0900

    Nestinterceptor를 구현받아 로깅 인터셉터 생성

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,13 @@ import { NestFactory, Reflector } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ClassSerializerInterceptor, ValidationPipe } from '@nestjs/common';
 import * as cookieParser from 'cookie-parser';
+import { LoggingInterceptor } from './middleware/logger.middleware';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
   // app.useGlobalInterceptors(new ClassSerializerInterceptor(app.get(Reflector)));
+  app.useGlobalInterceptors(new LoggingInterceptor());
   app.use(cookieParser());
   await app.listen(3000);
 }
